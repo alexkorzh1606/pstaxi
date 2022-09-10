d74679869ea68e9324fe675b19d1cfb6de60421f
Alex Korzh <alex.korzh@hotmail.com>
2022-09-10 18:09:05 +0300

    second commit

--- motorpool/admin.py
+++ motorpool/admin.py
@@ -4,6 +4,60 @@ from . import models
 # Register your models here.
 
 
+@admin.register(models.Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+class AutoInstanceInline(admin.TabularInline):
+    model = models.Auto
+    extra = 0
+
+
 @admin.register(models.Brand)
 class BrandAdmin(admin.ModelAdmin):
+    inlines = [AutoInstanceInline]
+
+
+@admin.register(models.Option)
+class OptionsAdmin(admin.ModelAdmin):
     pass
+
+
+class EnginePowerFilter(admin.SimpleListFilter):
+    title = 'Мощность двигателя'
+    parameter_name = 'engine_power_filter'
+
+    def lookups(self, request, model_admin):
+        filter_list = (
+            ('0', '0-100'),
+            ('1', '101-200'),
+            ('2', '201-300')
+        )
+        return filter_list
+
+    def queryset(self, request, queryset):
+        filter_value = self.value()
+        if filter_value == '0':
+            return queryset.filter(pts__engine_power__gte=0, pts__engine_power__lte=100)
+        if filter_value == '1':
+            return queryset.filter(pts__engine_power__gte=101, pts__engine_power__lte=200)
+        if filter_value == '0':
+            return queryset.filter(pts__engine_power__gte=201, pts__engine_power__lte=300)
+        return queryset
+
+
+@admin.register(models.Auto)
+class AutoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'number', 'brand', 'year', 'auto_class', 'display_engine_power', ]
+    list_select_related = ['brand', 'pts', ]
+    list_display_links = ['id', 'number', 'brand', ]
+    list_filter = [EnginePowerFilter, 'auto_class', 'options', ]
+    search_fields = ['brand__title', 'number', ]
+    readonly_fields = ['id', ]
+
+@admin.register(models.VehiclePassport)
+class VehiclePassportAdmin(admin.ModelAdmin):
+    list_display = ['id', 'auto', 'vin', 'engine_volume', 'engine_power']
+    list_filter = ['auto__brand', ]
+    list_select_related = ['auto__brand', ]
--- motorpool/models.py
+++ motorpool/models.py
@@ -1,10 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from utils.models import generate_unique_slug
+
 
 # Create your models here.
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    description = models.TextField(max_length=1000, null=True, blank=True, verbose_name='О себе')
+    phone = models.CharField(max_length=30, null=True, blank=True, verbose_name='Телефон')
+    address = models.CharField(max_length=250, null=True, blank=True, verbose_name='Адрес')
+
+    class Meta:
+        verbose_name_plural = 'Профили'
+        verbose_name = 'Профиль'
+
+    def __str__(self):
+        return self.title
+
+
 class Brand(models.Model):
     title = models.CharField(max_length=104, verbose_name = 'Название')
+    slug = models.SlugField(max_length=210, default='', blank=True)
 
     class Meta:
         verbose_name_plural = 'Бренды'
@@ -12,3 +30,72 @@ class Brand(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = generate_unique_slug(Brand, self.title)
+        super().save(*args, *kwargs)
+
+
+class Option(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Опции'
+        verbose_name = 'Опция'
+
+
+class AutoManagerVolvo(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(brand__title='Volvo')
+
+
+class Auto(models.Model):
+    AUTO_CLASS_ECONOMY = 'e'
+    AUTO_CLASS_COMFORT = 'c'
+    AUTO_CLASS_BUSINESS = 'b'
+
+    AUTO_CLASS_CHOICES = (
+        (AUTO_CLASS_ECONOMY, 'economy'),
+        (AUTO_CLASS_COMFORT, 'comfort'),
+        (AUTO_CLASS_BUSINESS, 'business'),
+    )
+
+    brand = models.ForeignKey(Brand, null=True, on_delete=models.CASCADE, related_name='cars')
+    options = models.ManyToManyField(Option, related_name='cars')
+    number = models.CharField(max_length=15)
+    description = models.TextField(max_length=500, default='', blank=True)
+    year = models.PositiveSmallIntegerField(null=True)
+    auto_class = models.CharField(max_length=1, null=True, choices=AUTO_CLASS_CHOICES, default=AUTO_CLASS_ECONOMY)
+
+
+    def __str__(self):
+        return self.number
+
+    class Meta:
+        verbose_name_plural = 'Автомобили'
+        verbose_name = 'Автомобиль'
+
+    def display_options(self):
+        return ', '.join([options.title for options in self.options.all()[:3]])
+    display_options.short_description = 'Options'
+
+    def display_engine_power(self):
+        return self.pts.engine_power
+    display_engine_power.short_description = 'Engine power'
+
+
+class VehiclePassport(models.Model):
+    auto = models.OneToOneField(Auto, on_delete=models.CASCADE, related_name='pts', verbose_name=Auto._meta.verbose_name)
+    vin = models.CharField(max_length=30, verbose_name='Идентификационный номер (VIN)')
+    engine_volume = models.SmallIntegerField(verbose_name='Объем двигателя, куб.см')
+    engine_power = models.SmallIntegerField(verbose_name='Мощность двигателя, л.с.')
+
+    def __str__(self):
+        return f'{self.auto}::{self.vin}'
+
+    class Meta:
+        verbose_name_plural = 'Паспорта машин'
+        verbose_name = 'Паспорт машины'
